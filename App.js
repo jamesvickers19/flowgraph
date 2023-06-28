6b5701b63c02465425c5f626ad226969c43f2632
James Vickers <jamesvickers19@gmail.com>
2023-06-28 16:17:04 -0600

    initial expo setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>You know I'm something of a mobile app developer myself</Text>
       <StatusBar style="auto" />
     </View>
   );
