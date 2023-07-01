8bf4cec17626cd4129ba8068bfddcc108fe3df33
James Vickers <jamesvickers19@gmail.com>
2023-07-01 16:52:41 -0600

    refactor graph preprocessing so its done one time, not on re-render

--- src/components/graph_control.js
+++ src/components/graph_control.js
@@ -6,19 +6,18 @@ function nodeLookupByName(nodes) {
     return Object.assign({}, ...nodes.map((x) => ({ [x.name]: x })));
 }
 
-// TODO maybe do preprocessing in an initial function, then a render function that takes those?
 const GraphControl = (graph) => {
-    const { name, objects, edges } = graph;
-    const nameToNode = nodeLookupByName(objects);
+    const nameToNode = nodeLookupByName(graph.objects);
+    return RenderGraphControl({ nameToNode: nameToNode, ...graph });
+}
+
+const RenderGraphControl = (graph) => {
+    const { name, objects, nameToNode } = graph;
     const [currentNode, setCurrentNode] = useState(objects.filter(x => x["isStart"])[0]);
     const nodeCallback = (answer) => {
-        console.log(`nodeCallback: ${answer}`);
-        console.log(`rule: ${currentNode.rule}`);
         const rule = JSON.parse(currentNode.rule);
         const nextNodeName = jsonLogic.apply(rule, { "answer": answer });
-        console.log(`nextNodeName: ${nextNodeName}`);
         const nextNode = nameToNode[nextNodeName];
-        console.log(`nextNode: ${JSON.stringify(nextNode)}`);
         setCurrentNode(nextNode);
     }
 
